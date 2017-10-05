8fe51c0211778105a6954be7907ee728c9607ec3
Panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2017-10-04 23:32:55 -0700

    added First screen, progresss Bar and making a fetch to get the quizdata

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import * as Progress from "react-native-progress";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showProgressBar: true
+    };
+  }
+
+  startTrivia = () => {
+    console.warn("start the app");
+  };
+
+  loadData = async () => {
+    try {
+      let response = await fetch("http://dev.theappsdr.com/apis/trivia_json/index.php");
+      let responseJson = await response.json();
+      console.warn(JSON.stringify(responseJson));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  componentWillMount() {
+    this.loadData();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>Welcome to</Text>
+          <Text style={styles.headerText}>Trivia App</Text>
+
+          {this.state.showProgressBar && (
+            <View style={styles.progressBarContainer}>
+              <Progress.Circle size={70} indeterminate={true} />
+              <Text style={styles.progressTitle}> Loading Trivia </Text>
+            </View>
+          )}
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight style={styles.startButton} onPress={this.startTrivia}>
+            <Text> Start Trivia </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -15,9 +55,32 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 4,
+    marginTop: 30,
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
+  headerContainer: {
+    flex: 3,
+    alignItems: "center"
+  },
+  progressBarContainer: {
+    marginTop: 50,
+    alignItems: "center"
+  },
+  progressTitle: {
+    marginTop: 10
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 20
+  },
+  startButton: {
+    backgroundColor: "gray",
+    padding: 5
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: "center"
+  }
 });

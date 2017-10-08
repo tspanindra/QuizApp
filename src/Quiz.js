eb944f6a0dbce9e0b80a4a23dfdc51eab01015f5
Panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2017-10-07 22:40:43 -0700

    quiz loading done

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,40 +1,42 @@
 //import liraries
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableHighlight } from "react-native";
+import { View, Text, StyleSheet, TouchableHighlight, Image, ScrollView } from "react-native";
 import * as Progress from "react-native-progress";
 import { connect } from "react-redux";
-import RadioForm, {
-  RadioButton,
-  RadioButtonInput,
-  RadioButtonLabel
-} from "react-native-simple-radio-button";
+import RadioForm from "react-native-simple-radio-button";
 
 export const mapStateToProps = (state: Object) => {
   return {
-    questions: state.QuizApp
+    questions: state.QuizApp.questions
   };
 };
 
 // create a component
 class Quiz extends Component {
-  constructor() {
-    super();
-    this.state = {
-      showProgressBar: true,
-      value: 0
-    };
-  }
   static navigationOptions = {
     title: "Quiz",
     headerStyle: { paddingTop: 20 }
   };
 
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      questionIndex: 0
+    };
+  }
+
+  getRadioOptions() {
+    const choices = this.props.questions[this.state.questionIndex].choices;
+    let radio_props = [];
+
+    choices.choice.map(choice => {
+      radio_props.push({ label: choice, value: 0 });
+    });
+    return radio_props;
+  }
+
   render() {
-    const radio_props = [
-      { label: "param1", value: 0 },
-      { label: "param2", value: 0 },
-      { label: "param3", value: 0 }
-    ];
     return (
       <View style={styles.container}>
         <View style={styles.borderView}>
@@ -42,27 +44,41 @@ class Quiz extends Component {
           <Text style={styles.textStyle}>Time remaining: </Text>
         </View>
 
-        {this.state.showProgressBar && (
+        {!this.props.questions && (
           <View style={styles.progressBarContainer}>
             <Progress.Circle size={40} indeterminate={true} />
           </View>
         )}
-        <View>
-          <RadioForm
-            radio_props={radio_props}
-            initial={0}
-            labelHorizontal={true}
-            onPress={value => {
-              this.setState({ value: value });
-            }}
+        <View style={styles.imageContainer}>
+          <Image
+            style={{ width: 250, height: 150 }}
+            source={{ uri: this.props.questions[this.state.questionIndex].image }}
+            resizeMode={"contain"}
           />
         </View>
+
+        {this.props.questions && (
+          <ScrollView contentContainerStyle={styles.questionContainer}>
+            <Text>{this.props.questions[this.state.questionIndex].text} </Text>
+            <RadioForm
+              radio_props={this.getRadioOptions()}
+              initial={0}
+              style={{ alignItems: "flex-start" }}
+              labelHorizontal={true}
+              onPress={value => {
+                this.setState({ value: value });
+              }}
+            />
+          </ScrollView>
+        )}
 
         <View style={styles.buttonContainer}>
           <TouchableHighlight onPress={() => console.warn("clicked")}>
             <Text style={styles.buttonText}> Quit </Text>
           </TouchableHighlight>
-          <TouchableHighlight onPress={() => console.warn("clicked")}>
+          <TouchableHighlight
+            onPress={() => this.setState({ questionIndex: this.state.questionIndex + 1 })}
+          >
             <Text style={styles.buttonText}> Next </Text>
           </TouchableHighlight>
         </View>
@@ -94,6 +110,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     padding: 5
+  },
+  imageContainer: {
+    alignItems: "center",
+    padding: 10
+  },
+  questionContainer: {
+    alignItems: "flex-start",
+    marginTop: 20
   },
   buttonText: {
     padding: 5,
